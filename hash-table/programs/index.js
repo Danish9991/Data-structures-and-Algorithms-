245d4a67bea03bdef1c852872c200e6187cb4f67
macbook <danishmir9991@gmail.com>
2023-04-25 12:31:19 +0530

    updated

--- hash-table/programs/index.js
+++ hash-table/programs/index.js
@@ -1,14 +1,41 @@
 // WAP to return first recurring/repeating character from array
 
-const firstRecurringCharacter = (input) => {
-   for(let i = 0; i < input.length; i++) {
-    for(let j = 1; j < input.length; j++) {
-        if(input[i] === input[j]){
-            return input[i]
-        }
-    }
-   }
-   return undefined;
-}
+/**
+ * firstRecurringCharacter method return the repeating character from array
+ * Time Complexity O(n^2)
+ * Space Complexity O(1)
+ * @param {*} input
+ * @returns
+ */
 
-firstRecurringCharacter([2,5,1,1,2,4,9,6])
+const firstRecurringCharacter = (input) => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+};
+
+/**
+ * firstRecurringCharacter2 method using the efficient method to return first repeated element from an array
+ * Time Complexity O(n)
+ * Space Complexity O(n)
+ * @param {*} input
+ * @returns
+ */
+
+const firstRecurringCharacter2 = (input) => {
+  const map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]]) {
+      return input[i];
+    }
+    map[input[i]] = i;
+  }
+};
+
+console.log(firstRecurringCharacter([2, 5, 1, 1, 2, 4, 9, 6]));
+console.log(firstRecurringCharacter2([2, 5, 1, 1, 2, 4, 9, 6]));

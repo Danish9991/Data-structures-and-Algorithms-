ee75fd25d3755488b98028621c161bfa30b5d1ab
macbook <danishmir9991@gmail.com>
2023-04-26 09:19:38 +0530

    added common element prog present in arrays

--- hash-table/programs/index.js
+++ hash-table/programs/index.js
@@ -35,7 +35,54 @@ const firstRecurringCharacter2 = (input) => {
     }
     map[input[i]] = i;
   }
+  return undefined;
 };
 
 console.log(firstRecurringCharacter([2, 5, 1, 1, 2, 4, 9, 6]));
 console.log(firstRecurringCharacter2([2, 5, 1, 1, 2, 4, 9, 6]));
+
+//WAP to check whether the two array have any common item
+
+/**
+ * checkCommonItem method using an efficient approach to return true if any common element present in two arrays
+ * Time Complexity O(n)
+ * Space Complexity O(n)
+ * @param {*} input1
+ * @param {*} input2
+ * @returns
+ */
+
+const checkCommonItem = (input1, input2) => {
+  for (let i = 0; i < input1.length; i++) {
+    for (let j = 0; j < input2.length; j++) {
+      if (input1[i] === input2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+/**
+ * checkCommonItem2 method return true if any common element present in two arrays
+ * @param {*} input1
+ * @param {*} input2
+ * @returns
+ */
+
+const checkCommonItem2 = (input1, input2) => {
+  let map = {};
+  for (let i = 0; i < input1.length; i++) {
+    if (!map[input1[i]]) map[input1[i]] = true;
+  }
+
+  for (let j = 0; j < input2.length; j++) {
+    if (map[input2[j]]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(checkCommonItem([3, 9, 0, 4], [8, 5, 6, 2, 1])); //false
+console.log(checkCommonItem2([3, 9, 0, 4], [8, 5, 6, 0, 1])); //true

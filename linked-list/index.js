9227020c4b5922af5ee142d337e2dc7529802644
macbook <danishmir9991@gmail.com>
2023-04-30 10:37:06 +0530

    updated singly linked list

---
+++ linked-list/index.js
@@ -0,0 +1,10 @@
+import { SinglyLinkedList } from './singlyLinked.list.js'
+
+const myLinkedList = new SinglyLinkedList(10);
+myLinkedList.append(15);
+myLinkedList.append(20);
+myLinkedList.prepend(5);
+myLinkedList.insert(6, 17);
+myLinkedList.remove(0);
+console.log(myLinkedList);
+console.log(myLinkedList.printList());

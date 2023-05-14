eaaa60120a052fd265331a005371c7de29420718
macbook <danishmir9991@gmail.com>
2023-05-14 10:42:20 +0530

    added bubble sort

---
+++ algorithms/sorting/bubble-sort/bubble-sort.js
@@ -0,0 +1,27 @@
+/**
+ * BubbleSort class performs sorting of array through bubble sort technique
+ */
+
+export class BubbleSort {
+  constructor(array) {
+    this.arr = array;
+  }
+
+  sort() {
+    if (this.arr.length < 2) {
+      return this.arr;
+    }
+
+    for (let i = 0; i < this.arr.length; i++) {
+      for (let j = 0; j < this.arr.length - 1; j++) {
+        if (this.arr[j] > this.arr[j + 1]) {
+          let temp = this.arr[j];
+          this.arr[j] = this.arr[j + 1];
+          this.arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return this.arr;
+  }
+}
---
+++ algorithms/sorting/bubble-sort/index.js
@@ -0,0 +1,7 @@
+import { BubbleSort } from "./bubble-sort.js";
+
+const unsortedArray = [5, 2, 8, 1, 3];
+
+const bubbleSort = new BubbleSort(unsortedArray);
+
+console.log(bubbleSort.sort());
